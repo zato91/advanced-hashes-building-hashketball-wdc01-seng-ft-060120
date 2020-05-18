99bd9f32577895c20cb3f97baf6604b6f7698420
zara ahmadi <zatokey@gmail.com>
2020-05-18 04:08:58 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,11 +4,21 @@
 def game_hash 
   {
     home: {
+      team_name:"",
+      colors:[],
+      players:[ 
+        {},
+        ],
       
-    }
+    },
     away :{
+       team_name:"",
+      colors:[],
+      players:[ 
+        {},
+        ],
       
-    }
+    },
   }
     
   
