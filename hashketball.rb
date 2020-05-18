812bd5c82cb4a9ba15e7925f1c31cc59c3dc9b8e
zara ahmadi <zatokey@gmail.com>
2020-05-18 04:24:45 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -92,15 +92,15 @@ def game_hash
         slam_dunks:10
         },
         {
-        player_name:,
-        number:,
-        shoe:,
-        points:,
-        rebounds:,
-        assists:,
-        steals:,
-        block:,
-        slam_dunks:
+        player_name:"DeSagna Diop",
+        number:2,
+        shoe:14,
+        points:24,
+        rebounds:12,
+        assists:12,
+        steals:4,
+        block:5,
+        slam_dunks:5
         },
         {
         player_name:,
